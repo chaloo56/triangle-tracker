14ed771292476380fd648462c76578854b7acaa3
chaloo56 <chaloo56@gmail.com>
2019-06-02 18:05:42 +0300

    add property for intext display

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 var ln1 = parseInt(document.getElementById("a").value);
  var ln2 = parseInt(document.getElementById("b").value);
  var ln3 = parseInt(document.getElementById("c").value);
+ //defining the returning path
+ var para = document.querySelector('p');
